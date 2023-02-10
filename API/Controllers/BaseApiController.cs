4e1fba4467a77ae42fb6d9791a20c0d1cb17c728
jacob-aa <jacob.temple@aa.com>
2023-02-10 10:08:58 -0700

    End of Section 10

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +13,13 @@ namespace API.Controllers
 
         // if _mediator is null then set Mediator to...
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null) return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
