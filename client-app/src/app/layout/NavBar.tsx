48e7f6ac721d416020d4a90b8e2e435f4ebd3577
jacob-aa <jacob.temple@aa.com>
2023-02-13 16:07:54 -0700

    End of Section 13

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,9 +1,12 @@
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu, MenuItem } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Container, Menu, MenuItem, Image, Dropdown } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
 
 
-export default function NavBar() {
+export default observer(function NavBar() {
+    const {userStore: {user, logout}} = useStore();
 
     return(
         <Menu inverted fixed='top'>
@@ -17,7 +20,16 @@ export default function NavBar() {
                 <MenuItem>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </MenuItem>
+                <MenuItem position='right'>
+                    <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+                    <Dropdown pointing='top left' text={user?.displayName}>
+                        <Dropdown.Menu>
+                            <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+                            <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </MenuItem>
             </Container>
         </Menu>
     )
-}
+})
