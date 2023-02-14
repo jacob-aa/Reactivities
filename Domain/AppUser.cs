13406b3dca419f7ad1241d3226329cd882076bf1
jacob-aa <jacob.temple@aa.com>
2023-02-14 14:22:59 -0700

    end of section 14

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain
@@ -10,6 +7,8 @@ namespace Domain
     {
         public string DisplayName { get; set; }
         public string Bio { get; set; }
+
+        public ICollection<ActivityAttendee> Activities { get; set; }
         
     }
 }
