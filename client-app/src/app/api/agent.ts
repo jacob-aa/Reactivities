60ae4a4859ddf621d367b11d9ef7e0b514d7e0e7
jacob-aa <jacob.temple@aa.com>
2023-02-16 16:00:06 -0700

    end of section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
@@ -81,10 +82,23 @@ const Account = {
     login: (user: UserFormValues) => requests.post<User>('/account/login', user), 
     register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
+const Profiles = {
+    get: (username:string) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData,{
+            headers: {'Content-Type': 'multipar/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+}
 
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;
