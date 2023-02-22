817df104e10c926cad7e30bf0b9c8bfde2cf0e95
jacob-aa <jacob.temple@aa.com>
2023-02-22 11:24:11 -0700

    section 21 pre challenge

---
+++ Application/Core/PagingParams.cs
@@ -0,0 +1,15 @@
+namespace Application.Core
+{
+    public class PagingParams
+    {
+        private const int MaxPagSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPagSize) ? MaxPagSize : value;
+        }
+
+    }
+}
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { PaginatedResult } from '../models/pagination';
 import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { router } from '../router/Routes';
@@ -24,6 +25,11 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
         await sleep(1000);
+        const pagination = response.headers['pagination'];
+        if (pagination){
+            response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+            return response as AxiosResponse<PaginatedResult<any>>;
+        }
         return response;
 }, (error: AxiosError) => {
     const {data, status, config} = error.response as AxiosResponse;
@@ -69,7 +75,7 @@ const requests = {
 }
 
 const Activities = {
-    lists: () => requests.get<Activity[]>('/activities'),
+    lists: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', {params}).then(responseBody),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
     update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
@@ -88,7 +94,7 @@ const Profiles = {
         let formData = new FormData();
         formData.append('File', file);
         return axios.post<Photo>('photos', formData,{
-            headers: {'Content-Type': 'multipar/form-data'}
+            headers: {'Content-Type': 'multipart/form-data'}
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
